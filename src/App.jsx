cde57317d8caf607548787a91c3cdb8f3492f378
piyush960 <piyushdahake096@gmail.com>
2024-01-25 22:46:34 +0530

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-// rendering lists
+/**
+ React Hook -> special function that allow functional components to use react features
+              without writing classes (useState, useEffect, useContext, etc.) 
+  
+  useState() -> a react hook that allows creation of a stateful variable and a setter
+                setter is used to update the value of stateful variable in virtual DOM
+                [name, setName]
+ 
+ */
 
-import List from "./List"
+import Counter from './Counter'
 
 function App() {
 
-    const fruits = [{id: 1, name: "apple", calories: 95},
-                    {id: 2, name: "Mango", calories: 45},
-                    {id: 3, name: "Banana", calories: 105},
-                    {id: 4, name: "Pineapple", calories: 37},
-                    {id: 5, name: "Chickoo", calories: 86},
-                    {id: 6, name: "Grapes", calories: 75}]
-
-
-    const vegetables = [{id: 1, name: "Potato", calories: 95},
-                    {id: 2, name: "Tomato", calories: 45},
-                    {id: 3, name: "Carrot", calories: 105},
-                    {id: 4, name: "Brinjal", calories: 37},
-                    {id: 5, name: "Peas", calories: 86},
-                    {id: 6, name: "Lady Finger", calories: 75}]
-
-    return(
-      <>
-        <List category="Fruits" items={fruits}/>
-        <List category="Vegetables" items={vegetables}/>
-      </>
-    )
+  return(
+    <Counter />
+  )
 }
 
 export default App
