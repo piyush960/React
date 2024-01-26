d06af9a732191d8ae5aa7a175491172a979d1113
piyush960 <piyushdahake096@gmail.com>
2024-01-26 21:53:39 +0530

    Digital Clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-/*
-useEffect() -> React Hook that tells react to fire a function when
-1.) component re-renders -- useEffect(() => {})
-2.) when component is mounted -- useEffect(() => {}, [])
-3.) when state of value changes -- useEffect(() => {}, [value])
 
-// USES
-
-Event Listeners
-DOM manipulation
-Subscriptions (real - time updates)
-Fetching data from an API
-Clean up when a component is unmounted
-
-*/
-import MyComponent from "./MyComponent"
+import DigitalClock from "./DigitalClock"
 
 function App(){
-  return(<MyComponent />) 
+  return(<>
+          <DigitalClock />
+        </>)
 }
 
 export default App
