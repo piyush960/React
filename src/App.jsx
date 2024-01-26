0a8c17eba03e558f1a96daad14bfa094ffdc76e3
piyush960 <piyushdahake096@gmail.com>
2024-01-26 16:01:29 +0530

    Updater Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-// Color Picker App
+/*
+updater function -> it is passed as an argument to setState()
+it is used with multiple state updates and asynchronous functions
+allows safe update based on the previous state
 
-import ColorPicker from "./ColorPicker";
+it uses the current state to calculate the next state
+set function do not trigger an update
+React batches together setState() for performance reasons
+NEXT state is still the CURRENT state after an update
+
+but, 
+updater function takes the PENDING state to calculate the NEXT state
+React puts the updater functions in a queue (waiting in line)
+
+*/
+
+import MyComponent from "./MyComponent";
 
 function App(){
 
-  return(<ColorPicker />)
+  return(<MyComponent />)
 }
 
 export default App
